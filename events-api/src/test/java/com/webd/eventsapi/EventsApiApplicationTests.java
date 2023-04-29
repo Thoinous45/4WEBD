b8550cdb0f8e9b8dd7627562251aadc78772e7c9
Lucas <lucas.delaperriere@gmail.com>
2023-04-29 14:58:53 +0200

    Initialisation

---
+++ events-api/src/test/java/com/webd/eventsapi/EventsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webd.eventsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
