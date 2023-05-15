0f72a2dc1cfa59ffc94fc5a0f378097efe4bbffb
Lucas <lucas.delaperriere@gmail.com>
2023-05-15 16:31:37 +0200

    Service to service security

--- events-api/src/main/java/com/webd/eventsapi/security/SecurityConfig.java
+++ events-api/src/main/java/com/webd/eventsapi/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @EnableWebSecurity
@@ -18,15 +19,16 @@ import static org.springframework.http.HttpMethod.GET;
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final LocalhostFilter localhostFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterAfter(localhostFilter, JwtAuthenticationFilter.class)
                 .authorizeHttpRequests()
-                .requestMatchers(GET, "/api/events/**").permitAll()
-                .requestMatchers("/api/events/search").permitAll()
-                .requestMatchers(GET, "/api/event-types/**").permitAll()
+                .requestMatchers(GET, "/api/events/**", "/api/event-types/**").permitAll()
+                .requestMatchers(POST, "/api/events/search").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .build();
