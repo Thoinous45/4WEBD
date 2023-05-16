6f47f44b59496250d8475f59792f5fcaf7ed03ea
Lucas <lucas.delaperriere@gmail.com>
2023-05-16 22:24:06 +0200

    Update docker compose

--- events-api/src/main/java/com/webd/eventsapi/security/LocalhostFilter.java
+++ events-api/src/main/java/com/webd/eventsapi/security/LocalhostFilter.java
@@ -8,19 +8,25 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.util.List;
 
 @Component
 public class LocalhostFilter extends OncePerRequestFilter {
+
+    private final List<String> allowedHosts = List.of("tickets-api.booking");
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         String remoteAddr = request.getRemoteAddr();
+        InetAddress inetAddress = InetAddress.getByName(remoteAddr);
+        String hostName = inetAddress.getHostName();
 
-        if ("127.0.0.1".equals(remoteAddr) || "0:0:0:0:0:0:0:1".equals(remoteAddr) || "ticket-service".equals(remoteAddr)) {
+        if (allowedHosts.contains(hostName)) {
             filterChain.doFilter(request, response);
         } else {
-            throw new ServletException("Invalid request");
+            throw new ServletException("Access denied for host: " + hostName);
         }
     }
 
